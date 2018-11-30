c315a6feb3ae44d6b96db4902948b48be11df9a4
vesprada <vesprada@PC12-T224>
2018-11-30 20:43:49 +0100

    Primer commit 30/11/2018

--- htdocs/DWC/VUE/Actividad2/main.js
+++ htdocs/DWC/VUE/Actividad2/main.js
@@ -1,28 +1,52 @@
 'use strict'
 
+/**
+	Variable donde almacenaremos todo lo referente a la aplicación
+*/
 const STORE = {
+	debug: true, // Modo debug (Si esta activado manda mensajes por cada acción que se ejecuta)
 	state: {
 		almacen: getStorage(),
 	},
+	// Método que suma unidades a un producto del array en base a su codigo
 	sumar(cod) {
+		if (this.debug) {console.log('sumar triggered with', cod);}
 		this.state.almacen.filter((prod)=>prod.cod == cod)[0].units += 1;
 	},
+	// Método que resta unidades a un producto del array en base a su codigo
 	restar(cod) {
+		if (this.debug) {console.log('restar triggered with', cod);}
 		let prod = this.state.almacen.filter((prod)=>prod.cod == cod)[0].units -= 1;
 	},
+	// Método que borra un producto del array en base a su codigo
 	borrar(cod) {
+		if (this.debug) {console.log('borrar triggered with', cod);}
 		let prod = this.state.almacen.filter((prod)=>prod.cod == cod)[0];
 		if (confirm("¿Deseas borrar el producto " + prod.name + "?")) {
 			this.state.almacen.splice(this.state.almacen.indexOf(prod),1);
 		}
 	},
+	// Método que añade un nuevo producto al array
 	newProd(producto) {
+		if (this.debug) {console.log('newProd triggered with', producto);}
+		// Comprobamos que no haya un producto con ese codigo
 		let prod = this.state.almacen.filter((prod)=>prod.cod == producto.cod)[0];
 		if (!prod) {
 			this.state.almacen.push(producto);
+		} else {
+			alert("El codigo introducido ya está registrado");
 		}
-	}
+	},
+	// Método que guarda los cambios antes de cerrar la página
+	saveToStorage() {
+		localStorage.setItem('productos', JSON.stringify(this.state.almacen));
+	},
 }
+
+/*
+	Función que rellena el array de tareas desde el localStorage
+	Si este está vacio devuelve un array con datos de prueba
+*/
 
 function getStorage() {
 	if (localStorage.getItem('productos')) {
@@ -36,6 +60,9 @@ function getStorage() {
 	}
 }
 
+/*
+	COMPONENTE VUE - Formulario de Nuevo producto
+*/
 Vue.component('add-prod-form', {
 	template:
 '  <form id="new-prod" method="POST">'+
@@ -78,6 +105,10 @@ Vue.component('add-prod-form', {
 	}
 });
 
+/*
+	COMPONENTE VUE - Tabla de Productos
+*/
+
 Vue.component('tabla-prod', {
 	template: 
 '	<table class="table table-striped">'+
@@ -89,25 +120,24 @@ Vue.component('tabla-prod', {
 '			<th>Importe</th>'+
 '			<th>Acciones</th>'+
 '		</tr>'+
-'		<fila-prod @sumar="sumar" @restar="restar" @borrar="borrar" v-for="(prod, index) in prods" :key="index" :producto="prod"></fila-prod>'+
+'		<fila-prod v-for="(prod, index) in prods" :key="index" :producto="prod"></fila-prod>'+
 '	</table>',
 	data() {
 		return {
 			prods: STORE.state.almacen,
 		}
 	},
-	methods: {
-		sumar(cod) {
-			STORE.sumar(cod);
-		},
-		restar(cod) {
-			STORE.restar(cod);
-		},
-		borrar(cod) {
-			STORE.borrar(cod);
-		},
+	created() {
+		// Evento que se activa justo antes de cerrar la página
+		window.addEventListener('beforeunload', event => {
+			STORE.saveToStorage();
+		});
 	}
 });
+
+/*
+	COMPONENTE VUE - Fila de Producto
+*/
 
 Vue.component('fila-prod', {
 	props: ['producto'],
@@ -119,21 +149,31 @@ Vue.component('fila-prod', {
 '		<td>{{producto.price}} €</td>'+
 '		<td>{{producto.units*producto.price}} €</td>'+
 '		<td>'+
-'			<button @click="$emit(\'sumar\', producto.cod)" type="button" title="Añadir Unidades" class="btn btn-default btn-sm">'+
+'			<button @click="sumar(producto.cod)" type="button" title="Añadir Unidades" class="btn btn-default btn-sm">'+
 '				<span class="glyphicon glyphicon-chevron-up" style="font-size: 25px;">↑</span>'+
 '			</button>'+
-'			<button v-if="producto.units > 0" @click="$emit(\'restar\', producto.cod)" type="button" title="Restar Unidades" class="btn btn-default btn-sm">'+
+'			<button v-if="producto.units > 0" @click="restar(producto.cod)" type="button" title="Restar Unidades" class="btn btn-default btn-sm">'+
 '				<span class="glyphicon glyphicon-chevron-down" style="font-size: 25px;">↓</span>'+
 '			</button>'+
-'			<button v-else disabled @click="$emit(\'restar\', producto.cod)" type="button" title="Restar Unidades" class="btn btn-default btn-sm">'+
+'			<button v-else disabled @click="restar(producto.cod)" type="button" title="Restar Unidades" class="btn btn-default btn-sm">'+
 '				<span class="glyphicon glyphicon-chevron-down" style="font-size: 25px;">↓</span>'+
 '			</button>'+
-'			<button @click="$emit(\'borrar\', producto.cod)" type="button" title="Borrar producto" class="btn btn-default btn-sm">'+
+'			<button @click="borrar(producto.cod)" type="button" title="Borrar producto" class="btn btn-default btn-sm">'+
 '				<span class="glyphicon glyphicon-trash" style="font-size: 25px;">🗑</span> '+
 '			</button>'+
 '		</td>'+
 '	</tr>',
+	methods: {
+		// Metodos que llaman al almacen
+		sumar(cod) {STORE.sumar(cod);},
+		restar(cod) {STORE.restar(cod);},
+		borrar(cod) {STORE.borrar(cod);},
+	}
 });
+
+/*
+	Aplicación VUE
+*/
 
 var miApp = new Vue({
 	el: "#app",
